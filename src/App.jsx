d96a71eb06b7bdeee1376a3c8d1396eb2a914867
RahulBalhara <rahulbalhara821@gmail.com>
2025-03-30 13:36:38 +0530

    add hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import ButtonGradient from './assets/svg/ButtonGradient'
+import Hero from './components/Hero';
 
 import Header from './Header';
 function App() {
@@ -7,6 +8,7 @@ function App() {
      
       <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden" >
        <Header/>
+       <Hero/>
        
       </div>
 
