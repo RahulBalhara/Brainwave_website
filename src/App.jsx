693f1aa1c009fe98363defaf035b9f8f495a3090
RahulBalhara <rahulbalhara821@gmail.com>
2025-03-31 12:36:59 +0530

    add Benifits  section

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import ButtonGradient from './assets/svg/ButtonGradient'
+import Benefits from './components/Benefits';
 import Hero from './components/Hero';
 
 import Header from './Header';
@@ -9,6 +10,7 @@ function App() {
       <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden" >
        <Header/>
        <Hero/>
+       <Benefits/>
        
       </div>
 
